9594cfff5c57b9961eea055c44f06132497940d9
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-11-10 13:48:30 +0700

    Layout file

---
+++ index.js
@@ -0,0 +1,59 @@
+import pmx from "pmx"
+
+const Probe = pmx.probe()
+
+const options = {
+  // Options related to the display style on Keymetrics
+  widget: {
+    // Logo displayed
+    logo: "http://tinker.press/images/donuts-350x150.jpg",
+    // Module colors
+    // 0 = main element
+    // 1 = secondary
+    // 2 = main border
+    // 3 = secondary border
+    theme: ["#141A1F", "#222222", "#3ff", "#3ff"],
+
+    // Section to show / hide
+    el: {
+      probes: true,
+      actions: true
+    },
+
+    // Main block to show / hide
+    block: {
+      actions: false,
+      issues: true,
+      meta: true,
+
+      // Custom metrics to put in BIG
+      main_probes: ["test-probe"]
+    }
+  }
+}
+
+const callback = (err, conf) => {
+  let totalReq = null
+  // Regis metrics
+  Probe.metric({
+    name: "Apache Request",
+    value: function() {
+      return totalReq
+    },
+    alert: {
+      mode: "threshold",
+      value: 500,
+      msg: "Congratulation. So many requests hit to our server!",
+      action: function(val) {
+        console.log(`${val} requests hit to server`)
+      }
+    }
+  })
+
+  setInterval(function() {
+    // Count
+    totalReq++
+  }, 1000)
+}
+
+pmx.initModule(options, callback)
