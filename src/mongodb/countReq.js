b81dfaa799ff4a68b48bfec1ddf5f7137c130a49
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-11-11 11:02:56 +0700

    Update

--- src/mongodb/countReq.js
+++ src/mongodb/countReq.js
@@ -1,16 +1,39 @@
 import mongoose from "mongoose"
-import connect, { loadDefaultModels } from "./connect"
+import { connect, loadDefaultModels } from "./connect"
 import "./models/customView"
-
 const _ = console.log
+
 connect("xhprof")
 loadDefaultModels()
 
-export const countReq = () => {
+const getLastXSecs = (unit, value) => {
+  let minusVal = null
+  switch (unit) {
+    case "m": {
+      minusVal = value * 60
+      break
+    }
+    case "s": {
+      minusVal = value
+      break
+    }
+    case "ms": {
+      minusVal = value / 1000
+      break
+    }
+    default: {
+      minusVal = 0
+      break
+    }
+  }
+  return Math.floor(new Date().getTime() / 1000) - minusVal
+}
+
+const countReq = (unit, value) => {
   const CustomView = mongoose.model("CustomView")
-  const lastMinute = Math.floor(new Date().getTime() / 1000) - 60
-  const wait = CustomView.count({ timestamp: { $gt: lastMinute } }).exec()
-  return wait.catch(err => _(err))
+  const lastXSecs = getLastXSecs(unit, value)
+  const wait = CustomView.count({ timestamp: { $gt: lastXSecs } }).exec()
+  return wait.catch(err => err)
 }
 
 export default countReq
--- src/pmx/ApacheRequest.js
+++ src/pmx/ApacheRequest.js
@@ -1,11 +1,13 @@
 import pmx from "pmx"
 import countReq from "../mongodb/countReq"
+
+const _ = console.log
 const Probe = pmx.probe()
 
 const options = {
   // Options related to the display style on Keymetrics
   widget: {
-    logo: "http://tinker.press/images/donuts-350x150.jpg",
+    logo: "https://storage.googleapis.com/glass-turbine-148103.appspot.com/apache.png",
     theme: ["#141A1F", "#222222", "#3ff", "#3ff"],
     // Section to show / hide
     el: {
@@ -15,25 +17,32 @@ const options = {
     // Main block to show / hide
     block: {
       actions: false,
-      issues: true,
-      meta: true,
+      issues: false,
+      meta: false,
+      cpu: false,
+      mem: false,
       // Custom metrics to put in BIG
-      main_probes: ["Apache Request"]
+      main_probes: ["Request"]
     }
   }
 }
 
 let totalReq = null
 
-pmx.initModule(options, () => {
+pmx.initModule(options, (err, conf) => {
   // Regis metrics
   Probe.metric({
-    name: "Apache Request",
+    name: "Request",
     value: () => totalReq
   })
-})
 
-setInterval(() => {
-  const countWait = countReq()
-  countWait.then(count => (totalReq = count))
-}, 1000)
+  const { interval_unit, interval_value } = conf
+
+  setInterval(() => {
+    const countWait = countReq(interval_unit, interval_value)
+    countWait.then(count => {
+      const value = interval_value === 1 ? "" : interval_value
+      totalReq = `${count}/${value}${interval_unit}`
+    })
+  }, 1000)
+})
